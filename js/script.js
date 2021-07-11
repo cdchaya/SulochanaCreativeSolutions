60a9436b361e8d93bf9cd87c10171b106e206ec1
devi ks <cd.chaya@gmail.com>
2021-07-11 19:50:10 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,33 @@
+$(window).scroll(function(){
+    var wscroll = $(this).scrollTop();
+    if(wscroll > 50){
+     $(".navbar").addClass("shrink-nav");
+    }
+    else{
+      $(".navbar").removeClass("shrink-nav");
+    }
+  });
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+  
